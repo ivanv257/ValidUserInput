b320739ce13974091c3a1ec2d9c56a9df9d18467
Ivan van der Merwe <ivanv257@gmail.com>
2018-08-01 07:21:29 +0200

    Update userinput.js

--- userinput.js
+++ userinput.js
@@ -3,7 +3,7 @@ let name = "";
 do {
 	name = prompt("Please enter your name: ");
 	}
-while(!name);
+while(!name); // if user accidently hits enter it will request for name again. 
 
 func1 = () => {
 	for (let i = 0, n = name.length; i < n; i++)
